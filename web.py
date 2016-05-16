0ef94d14a8bd9a4adc3b9e7141a1de9e1a7dce78
Yaroslav <yaroslav.bondarenko@frtk.ru>
2016-05-16 21:32:08 +0300

    I guess this time it would save

--- web.py
+++ web.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, g, flash
 from database import DBHandler
-import re, paramiko
+import re
+import paramiko
+import threading
 from configparser import ConfigParser
+import queue
+import logging
+
 
 # DEB: /etc/default/iptables
 # RPM: /etc/sysconfig/iptables
@@ -11,6 +16,24 @@ app.config.from_pyfile('flask.conf')
 regexpip = "^(((25[1-5])|(2[1-4][0-9])|([0-1]?[0-9]{1,2}))\.){3,3}\
 ((25[1-5])|(2[1-4][0-9])|([0-1]?[0-9]{1,2}))$"
 parser = ConfigParser()
+
+
+def log(s):
+    parser.read(filenames='settings.conf')
+    lg = parser.get(section="COMMON", option="logfile")
+    logging.basicConfig(filename=lg, level=logging.DEBUG)
+    flash(s)
+    logging.debug(s)
+
+
+def log_tuple(q):
+    parser.read(filenames='settings.conf')
+    lg = parser.get(section="COMMON", option="logfile")
+    logging.basicConfig(filename=lg, level=logging.DEBUG)
+    for s in q:
+        flash(s)
+        logging.debug(s)
+
 
 @app.before_request
 def prepare():
@@ -49,16 +72,16 @@ def edit_router(name):
     new_name = request.form.get('name', None)
     ip = request.form.get('ip', None)
     if name is None or ip is None:
-        flash('Smth bad has happened')
+        log('Smth bad has happened')
         return redirect(url_for('redirect_edit_router', name=name))
     elif len(new_name) == 0:
-        flash("Empty name")
+        log("Empty name")
         return redirect(url_for('redirect_edit_router', name=name))
     elif re.match(regexpip, ip) is None:
-        flash("Bad IP address")
+        log("Bad IP address")
         return redirect(url_for('redirect_edit_router', name=name))
     else:
-        flash(g.database.edit_router(name, ip, new_name))
+        log(g.database.edit_router(name, ip, new_name))
         return redirect(url_for('main'))
 
 
@@ -68,16 +91,16 @@ def edit_switching(port):
     ip = request.form.get('ip', None)
     new_p = request.form.get('int_p', None)
     if new_ep is None or ip is None or new_p is None:
-        flash('Smth bad has happened')
+        log('Smth bad has happened')
         return redirect(url_for('redirect_edit_switching', port=port))
     elif new_ep.isdigit() is False:
-        flash("Bad external port")
+        log("Bad external port")
         return redirect(url_for('redirect_edit_switching', port=port))
     elif new_p.isdigit() is False:
-        flash("Bad internal port")
+        log("Bad internal port")
         return redirect(url_for('redirect_edit_switching', port=port))
     elif re.match(regexpip, ip) is None:
-        flash("Bad IP address")
+        log("Bad IP address")
         return redirect(url_for('redirect_edit_switching', port=port))
     else:
         flash(g.database.edit_switching(port, new_ep, ip, new_p))
@@ -99,13 +122,13 @@ def add_router():
     name = request.form.get('name', None)
     ip = request.form.get('ip', None)
     if name is None or ip is None:
-        flash('Smth bad has happened')
+        log('Smth bad has happened')
     elif len(name) == 0:
-        flash('No name supplied')
+        log('No name supplied')
     elif re.match(regexpip, ip) is None:
-        flash("Bad IP address")
+        log("Bad IP address")
     else:
-        flash(g.database.add_router(name, ip))
+        log(g.database.add_router(name, ip))
     return redirect(url_for('main'))
 
 
@@ -115,82 +138,76 @@ def add_switching():
     ip = request.form.get('ip', None)
     int_p = request.form.get('int_p', None)
     if ext_p is None or ip is None or int_p is None:
-        flash('Smth bad has happened')
+        log('Smth bad has happened')
     elif ext_p.isdigit() is False:
-        flash("Bad external port")
+        log("Bad external port")
     elif int_p.isdigit() is False:
-        flash("Bad internal port")
+        log("Bad internal port")
     elif re.match(regexpip, ip) is None:
-        flash("Bad IP address")
+        log("Bad IP address")
     else:
-        flash(g.database.add_switching(ext_p, ip, int_p))
+        log(g.database.add_switching(ext_p, ip, int_p))
     return redirect(url_for('main'))
+
+
+def ssh_single(ip):
+    parser.read(filenames='settings.conf')
+    path = parser.get(section="SSH", option="path")
+    outif = parser.get(section="SSH", option="output_if")
+    inif = parser.get(section="SSH", option="input_if")
+    ssh_port = parser.getint(section="SSH", option="port")
+    login = parser.get(section="SSH", option="login")
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    key = paramiko.RSAKey.from_private_key_file(parser.get(section="SSH", option="keyfile"))
+    client.connect(hostname=ip, username=login, pkey=key, port=ssh_port, timeout=30)
+    fclient = client.open_sftp()
+    file = fclient.open(path, "w")
+    res = [{'ep': line[0], 'ip': line[1], 'p': line[2]} for line in g.database.get_switchings()]
+    file.writelines(render_template('iptables_template', param=res, outif=outif, inif=inif, eip=ip))
+    file.flush()
+    file.close()
+    fclient.close()
+    client.exec_command('iptables-restore < {path}'.format(path=path))
+    client.close()
 
 
 @app.route('/get/ssh/send/<string:router>', methods=["GET"])
 def ssh_send(router):
-    return render_template('ssh_info.html', hostname=router)
+    try:
+        rt, ip = g.database.get_router(router)
+        ssh_single(ip)
+        log("Successful sending for {name}".format(name=router))
+    except OSError as err:
+        log(err.__str__() + " for {name}".format(name=router))
+    except paramiko.AuthenticationException as err:
+        log(err.__str__() + " for {name}".format(name=router))
+    return redirect(url_for('main'))
 
 
-@app.route('/post/ssh/put/<string:router>', methods=["POST"])
-def ssh_put(router):
-    login = request.form.get('login', None)
-    pw = request.form.get('pass', None)
-    if login is None or pw is None:
-        flash('Smth bad has happened')
-    else:
+@app.route('/get/ssh/send_all', methods=["GET"])
+def ssh_send_all():
+    res = g.database.get_routers()
+    for src in res:
         try:
-            parser.read(filenames='settings.conf')
-            ssh_port = parser.getint(section="SSH", option="port")
-            path = parser.get(section="SSH", option="path")
-            outif = parser.get(section="SSH", option="output_if")
-            inif = parser.get(section="SSH", option="input_if")
-            client = paramiko.SSHClient()
-            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            rt, ip = g.database.get_router(router)
-            client.connect(hostname=ip, username=login, password=pw, port=ssh_port)
-            client.exec_command('')
-            fclient = client.open_sftp()
-            file = fclient.open(path, "w")
-            res = [{'ep': line[0], 'ip': line[1], 'p': line[2]} for line in g.database.get_switchings()]
-            # file.write('*nat\n')
-            # file.write(":PREROUTING ACCEPT [0:0]\n")
-            # file.write(":INPUT ACCEPT [0:0]\n")
-            # file.write(":OUTPUT ACCEPT [0:0]\n")
-            # file.write(":POSTROUTING ACCEPT [0:0]\n")
-            #
-            # for line in sw:
-            #     file.write("-A PREROUTING -d {eip}/32 -p tcp -m tcp --dport {ep}"
-            #                 " -j DNAT --to-destination {ip}:{p}\n".format(eip=ip, ep=line[0],
-            #                                                             ip=line[1], p=line[2]))
-            # file.write("-A POSTROUTING -p tcp -m conntrack --ctstate DNAT -o {int}"
-            #            " -j MASQUERADE\n".format(int=outif))
-            # file.write("-A POSTROUTING -p tcp -m tcp -o {int} -j MASQUERADE\n".format(int=inif))
-            # file.write("COMMIT\n")
-
-            file.writelines(render_template('iptables_template', param=res, outif=outif, inif=inif, eip=ip))
-            file.flush()
-            file.close()
-            fclient.close()
-            client.exec_command('iptables-restore < {path}'.format(path=path))
-
-            flash("Successful sending")
+            ssh_single(src[1])
+            log("Successful sending for {name}".format(name=src[0]))
         except OSError as err:
-            flash(err.__str__())
+            log(err.__str__() + " for {name}".format(name=src[0]))
         except paramiko.AuthenticationException as err:
-            flash(err.__str__())
+            log(err.__str__() + " for {name}".format(name=src[0]))
     return redirect(url_for('main'))
 
 
 @app.route('/get/router/delete/<string:router>', methods=['GET'])
 def delete_router(router):
-    flash(g.database.delete_router(router))
+    log(g.database.delete_router(router))
     return redirect(url_for('main'))
 
 
 @app.route('/get/switching/delete/<string:port>', methods=['GET'])
 def delete_switching(port):
-    flash(g.database.delete_switching(port))
+    log(g.database.delete_switching(port))
     return redirect(url_for('main'))
 
 
